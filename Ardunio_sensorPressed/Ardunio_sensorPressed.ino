a9eed4aa67272fd8aa1d888064b935d56cd8ae13
sofiapardillo <topaz1197@gmail.com>
2017-02-14 10:04:16 -0500

    first commit

---
+++ Ardunio_sensorPressed/Ardunio_sensorPressed.ino
@@ -0,0 +1,15 @@
+// Reads analogue sensor and writes value to the serial port
+
+int sensorPin = 0; //sensor hooked up to analog pin 0
+int val = 0;
+
+void setup() {
+Serial.begin(9600); // Start serial communication at 9600 bps
+}
+
+void loop() {
+val = analogRead(sensorPin); //read sensor and assign to variable called val
+delay(100); // Wait 100 milliseconds
+Serial.write(val);
+}
+
